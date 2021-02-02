77bbd5809421b0ba98e0e32373d66c745923ddf0
formatting with black on GCB <marufeuille@gmail.com>
2021-02-02 15:30:11 +0000

    formatting with black 20210202

--- black/black.sh
+++ black/black.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 mkdir -p ~/.ssh
-cat $(gcloud secrets versions access 1 --secret="GITHUB_DEPLOY_KEY" --project "ishii-sandbox") > ~/.ssh/id_rsa
+cp id_rsa ~/.ssh/
+chmod 600 ~/.ssh
+chmod 400 ~/.ssh/id_rsa
+cat - << EOS >> ~/.ssh/config
+Host github.com
+  HostName github.com
+  IdentityFile ~/.ssh/id_rsa
+  User git
+EOS
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+cat ~/.ssh/id_rsa
 git config user.name "formatting with black on GCB"
 git config user.email "marufeuille@gmail.com"
 black .
 git add -A
-git commit -m "formatting with black $(date+%Y%m%d)"
+git commit -m "formatting with black $(date +%Y%m%d)"
 git push
